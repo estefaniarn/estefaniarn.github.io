281b917f192348d964dad28ad98ec3f2cc47e929
Estefania <estefaniaroldann@gmail.com>
2025-05-06 09:21:16 -0600

    may6

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -44,7 +45,10 @@ def calculate():
 #     return render_template("result.html", total=total_emissions)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 
